7b6371b3b987fae0aa0d5f06b6e51d50054147c1
jemer2407 <jemer2407@gmail.com>
2024-11-22 13:27:56 +0100

    update

--- forecasts/views.py
+++ forecasts/views.py
@@ -21,34 +21,31 @@ def get_matches_both_team(obj):
     matches.append(visit_home)
     # visitante como visitante partidos jugados
     visit_visit = Match.objects.filter(visit_team=obj.visit_team).filter(gol_home_ht__gt=-1)
-
+    matches.append(visit_visit)
     num_total_match_home_home,num_match_win_home_home, num_match_lose_home_home, num_match_empate_home_home = calcular_num_match_1x2(home_home, 'home_home')
     num_total_match_home_visit,num_match_win_home_visit, num_match_lose_home_visit, num_match_empate_home_visit = calcular_num_match_1x2(home_visit, 'home_visit')
     num_total_match_visit_home,num_match_win_visit_home, num_match_lose_visit_home, num_match_empate_visit_home = calcular_num_match_1x2(visit_home, 'visit_home')
     num_total_match_visit_visit,num_match_win_visit_visit, num_match_lose_visit_visit, num_match_empate_visit_visit = calcular_num_match_1x2(visit_visit, 'visit_visit')
     
     # calculamos numero de partidos gana,pierde y empata local_local, local_visitante, visitante_local, visitante_visitante
-    # he añadido numero total de partidos jugados local como local, local como visitante, etc
-    # hay que calcular de nuevo las probabilidades teniendo en cuenta el numero total de partidos en cada caso
-    num_total_match_win_home = num_match_win_home_home + num_match_win_home_visit
-    num_total_match_lose_visit = num_match_lose_visit_home + num_match_lose_visit_visit
+    # numero de partidos para gana local
+    
+    num_total_match_win_home = num_match_win_home_home + num_match_win_home_visit# numero de partidos que ha ganado el local
+    num_total_match_lose_visit = num_match_lose_visit_home + num_match_lose_visit_visit# numero de partidos que ha perdido el visitante
+    
 
+    # numero de partidos para gana visitante
     num_total_match_win_visit = num_match_win_visit_home + num_match_win_visit_visit
     num_total_match_lose_home = num_match_lose_home_home + num_match_lose_home_visit
+
 
     num_total_match_empate_home = num_match_empate_home_home + num_match_empate_home_visit
     num_total_match_empate_visit = num_match_empate_visit_home + num_match_empate_visit_visit
 
-    print(num_total_match_win_home)
-    print(num_total_match_lose_visit)
+    num_total_match_home = num_total_match_home_home + num_total_match_home_visit# numero total de partidos que ha jugado el local
+    num_total_match_visit = num_total_match_visit_home + num_total_match_visit_visit# numero total de partidos que ha jugado el visitante
 
-    print(num_total_match_win_visit)
-    print(num_total_match_lose_home)
-
-    print(num_total_match_empate_home)
-    print(num_total_match_empate_visit)
-
-    matches.append(visit_visit)
+    
     num_match_over_05_ht = 0
     num_match_over_15_ht = 0
     num_match_over_05_ft = 0
@@ -75,7 +72,7 @@ def get_matches_both_team(obj):
         
         total_matches += len(m)
 
-    return total_matches,num_match_over_05_ht,num_match_over_15_ht,num_match_over_05_ft,num_match_over_15_ft,num_match_over_25_ft,num_match_over_35_ft,num_match_over_05_mitad2,num_match_aem_ft,num_match_aem_ht,num_match_aem_ht2,num_total_match_win_home,num_total_match_lose_visit,num_total_match_win_visit,num_total_match_lose_home,num_total_match_empate_home,num_total_match_empate_visit
+    return total_matches,num_match_over_05_ht,num_match_over_15_ht,num_match_over_05_ft,num_match_over_15_ft,num_match_over_25_ft,num_match_over_35_ft,num_match_over_05_mitad2,num_match_aem_ft,num_match_aem_ht,num_match_aem_ht2,num_total_match_win_home,num_total_match_lose_visit,num_total_match_win_visit,num_total_match_lose_home,num_total_match_empate_home,num_total_match_empate_visit,num_total_match_home,num_total_match_visit
         
 
 # funcion que calcula el numero de partidos que se ha dado el over
@@ -192,12 +189,12 @@ class MatchDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Pronosticos del '
+        context['title'] = 'Pronósticos para     '
 
         # lógica para calcular las probabilidades de los diferentes mercados
         # ------------------------------------ over ht y ft ----------------------------------------
         
-        total_matches, num_match_over_05_ht, num_match_over_15_ht, num_match_over_05_ft, num_match_over_15_ft, num_match_over_25_ft, num_match_over_35_ft, num_match_over_05_mitad2, num_match_aem_ft, num_match_aem_ht, num_match_aem_ht2,num_total_match_win_home,num_total_match_lose_visit,num_total_match_win_visit,num_total_match_lose_home,num_total_match_empate_home,num_total_match_empate_visit = get_matches_both_team(self.get_object())
+        total_matches, num_match_over_05_ht, num_match_over_15_ht, num_match_over_05_ft, num_match_over_15_ft, num_match_over_25_ft, num_match_over_35_ft, num_match_over_05_mitad2, num_match_aem_ft, num_match_aem_ht, num_match_aem_ht2,num_total_match_win_home,num_total_match_lose_visit,num_total_match_win_visit,num_total_match_lose_home,num_total_match_empate_home,num_total_match_empate_visit,num_total_match_home,num_total_match_visit = get_matches_both_team(self.get_object())
             
             
         prob_over_05_ht = np.round(100 * num_match_over_05_ht / total_matches,2)
@@ -213,38 +210,66 @@ class MatchDetailView(DetailView):
 
         # corregir --> numero de partidos total jugados si ha jugado cada equipo
 
-        prob_win_home = (np.round(100 * num_total_match_win_home / total_matches, 2) + np.round(100 * num_total_match_lose_visit / total_matches, 2))/2
-        prob_win_visit = (np.round(100 * num_total_match_win_visit / total_matches, 2) + np.round(100 * num_total_match_lose_home / total_matches, 2))/2
-        prob_empate = (np.round(100 * num_total_match_empate_home / total_matches, 2) + np.round(100 * num_total_match_empate_visit / total_matches, 2))/2
+        prob_win_home = (np.round(100 * num_total_match_win_home / num_total_match_home, 2) + np.round(100 * num_total_match_lose_visit / num_total_match_visit, 2))/2
+        prob_win_visit = (np.round(100 * num_total_match_win_visit / num_total_match_visit, 2) + np.round(100 * num_total_match_lose_home / num_total_match_home, 2))/2
+        prob_empate = (np.round(100 * num_total_match_empate_home / num_total_match_home, 2) + np.round(100 * num_total_match_empate_visit / num_total_match_visit, 2))/2
 
-        context['prob_win_home'] = prob_win_home
+        context['prob_win_home'] = round(prob_win_home,2)
         context['cuota_win_home'] = np.round((1/prob_win_home)*100,2)
-        context['prob_win_visit'] = prob_win_home
+        context['prob_win_visit'] = round(prob_win_visit,2)
         context['cuota_win_visit'] = np.round((1/prob_win_visit)*100,2)
-        context['prob_empate'] = prob_win_home
+        context['prob_empate'] = round(prob_empate,2)
         context['cuota_empate'] = np.round((1/prob_empate)*100,2)
 
-        context['prob_over_05_ht'] = prob_over_05_ht
+        context['prob_over_05_ht'] = round(prob_over_05_ht,2)
         context['cuota_over_05_ht'] = np.round((1/prob_over_05_ht)*100,2)
-        context['prob_over_15_ht'] = prob_over_15_ht
-        context['cuota_over_15_ht'] = np.round((1/prob_over_15_ht)*100,2)
+        context['prob_under_05_ht'] = round(100-prob_over_05_ht,2)
+        context['cuota_under_05_ht'] = np.round((1/(100 - prob_over_05_ht))*100,2)
 
-        context['prob_over_05_ft'] = prob_over_05_ft
+        context['prob_over_15_ht'] = round(prob_over_15_ht,2)
+        context['cuota_over_15_ht'] = np.round((1/prob_over_15_ht)*100,2)
+        context['prob_under_15_ht'] = round(100 - prob_over_15_ht,2)
+        context['cuota_under_15_ht'] = np.round((1/(100 - prob_over_15_ht))*100,2)
+
+        context['prob_over_05_ft'] = round(prob_over_05_ft,2)
         context['cuota_over_05_ft'] = np.round((1/prob_over_05_ft)*100,2)
-        context['prob_over_15_ft'] = prob_over_15_ft
+        context['prob_under_05_ft'] = round(100-prob_over_05_ft,2)
+        context['cuota_under_05_ft'] = np.round((1/(100 - prob_over_05_ft))*100,2)
+
+        context['prob_over_15_ft'] = round(prob_over_15_ft,2)
         context['cuota_over_15_ft'] = np.round((1/prob_over_15_ft)*100,2)
-        context['prob_over_25_ft'] = prob_over_25_ft
+        context['prob_under_15_ft'] = round(100 - prob_over_15_ft,2)
+        context['cuota_under_15_ft'] = np.round((1/(100 - prob_over_15_ft))*100,2)
+
+        context['prob_over_25_ft'] = round(prob_over_25_ft,2)
         context['cuota_over_25_ft'] = np.round((1/prob_over_25_ft)*100,2)
-        context['prob_over_35_ft'] = prob_over_35_ft
+        context['prob_under_25_ft'] = round(100 - prob_over_25_ft,2)
+        context['cuota_under_25_ft'] = np.round((1/(100 - prob_over_25_ft))*100,2)
+
+        context['prob_over_35_ft'] = round(prob_over_35_ft,2)
         context['cuota_over_35_ft'] = np.round((1/prob_over_35_ft)*100,2)
-        context['prob_over_05_mitad2'] = prob_over_05_mitad2
+        context['prob_under_35_ft'] = round(100 - prob_over_35_ft,2)
+        context['cuota_under_35_ft'] = np.round((1/(100 - prob_over_35_ft))*100,2)
+
+        context['prob_over_05_mitad2'] = round(prob_over_05_mitad2,2)
         context['cuota_over_05_mitad2'] = np.round((1/prob_over_05_mitad2)*100,2)
-        context['prob_aem_ft'] = prob_aem_ft
+        context['prob_under_05_mitad2'] = round(100 - prob_over_05_mitad2,2)
+        context['cuota_under_05_mitad2'] = np.round((1/(100 - prob_over_05_mitad2))*100,2)
+
+        context['prob_aem_ft'] = round(prob_aem_ft,2)
         context['cuota_aem_ft'] = np.round((1/prob_aem_ft)*100,2)
-        context['prob_aem_ht'] = prob_aem_ht
+        context['prob_aem_ft_no'] = round(100 - prob_aem_ft,2)
+        context['cuota_aem_ft_no'] = np.round((1/(100 - prob_aem_ft))*100,2)
+
+        context['prob_aem_ht'] = round(prob_aem_ht,2)
         context['cuota_aem_ht'] = np.round((1/prob_aem_ht)*100,2)
-        context['prob_aem_ht2'] = prob_aem_ht2
+        context['prob_aem_ht_no'] = round(100 - prob_aem_ht,2)
+        context['cuota_aem_ht_no'] = np.round((1/(100 - prob_aem_ht))*100,2)
+
+        context['prob_aem_ht2'] = round(prob_aem_ht2,2)
         context['cuota_aem_ht2'] = np.round((1/prob_aem_ht2)*100,2)
+        context['prob_aem_h2t_no'] = round(100 - prob_aem_ht2,2)
+        context['cuota_aem_ht2_no'] = np.round((1/(100 - prob_aem_ht2))*100,2)
 
 
 
